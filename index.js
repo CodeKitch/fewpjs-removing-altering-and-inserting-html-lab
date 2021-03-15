364f7e548df0331f4cdcef6f982f02801b00824d
JaSON <masteraccounthub@gmail.com>
2021-03-15 08:18:59 -0400

    commit tree ab9a375d738fe05f7272899c4ffd92300ba6b6ae

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Write your code here!
+
+// let indes = document.querySelector("main");
+// indes.remove();
+
+let indes = document.getElementById('main');
+indes.remove();
+
+// let newHeader = document.querySelector('h1#victory');
+// newHeader.innerHTML = 'Ye is the champion!'
+
+let newHeader = document.createElement('h1');
+let yourName = document.createTextNode('Ye is the champion!');
+newHeader.appendChild(yourName);
+newHeader.id = "victory"
+
+
